c320000a37a90d8adf15b0d3888a7d99b2c27a9f
JosephValle <josephv2599@gmail.com>
2023-06-07 09:27:04 -0400

    Init

---
+++ lib/bloc/home/home_bloc.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeUnloaded()) {
+    on<GetMedia>((event, emit) async {
+      try {
+        emit(HomeLoading());
+
+        emit(HomeLoaded());
+      } catch(e) {
+        emit(HomeError());
+      }
+    });
+  }
+}
---
+++ lib/bloc/home/home_event.dart
@@ -0,0 +1,11 @@
+part of 'home_bloc.dart';
+
+@immutable
+abstract class HomeEvent {}
+
+class GetMedia extends HomeEvent {
+  final String uniqueId;
+  final BuildContext context;
+
+  GetMedia({required this.uniqueId, required this.context});
+}
